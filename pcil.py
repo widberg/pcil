a67ad69bcd956d09b71cfc69d5a6115b5d0904e2
ProWolf <prowolfmailbox@gmail.com>
2018-08-24 17:14:54 -0400

    Added header generator

--- pcil.py
+++ pcil.py
@@ -1,12 +1,14 @@
 from argparse import ArgumentParser
 from urllib.request import urlretrieve
 import sys
+import os
+import re
 
 parser = ArgumentParser()
-parser.add_argument("-g", "--gpuonly", action="store_true", dest="gpuonly",
+parser.add_argument("-g", "--gpu", action="store_true", dest="gpuonly",
                     default=False, help="only write gpu info to the header file")
-parser.add_argument("-p", "--path", dest="path", default="pcil.h",
-                    help="PATH to output the header file", metavar="PATH")
+parser.add_argument("-o", "--out", dest="path", default="pcil.h",
+                    help="Path to output the header file", metavar="PATH")
 parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
                     default=False, help="don't print status messages to stdout")
 parser.add_argument("-u", "--url", dest="url",
@@ -28,10 +30,45 @@ def reporthook(blocknum, blocksize, totalsize):
 
 
 if __name__ == '__main__':
-    if args.download:
+    path = os.path.join(os.path.dirname(__file__), args.path)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    if args.url:
         if args.quiet:
-            urlretrieve(args.download, 'pci.ids')
+            urlretrieve(args.url, 'pci.ids')
         else:
-            print("Downloading id database from " + args.download)
-            urlretrieve(args.download, 'pci.ids', reporthook)
+            print("Downloading id database from " + args.url)
+            urlretrieve(args.url, 'pci.ids', reporthook)
             print("Download complete")
+    with open(path, "w", encoding="utf8") as headerfile:
+        headerfile.write("""#ifndef PCIL_H
+#define PCIL_H
+#include <map>
+
+namespace pcil
+{
+    static const std::map<short, char*> names = {
+""")
+        with open(os.path.join(os.path.dirname(__file__), "pci.ids"), "r", encoding="utf8") as pcifile:
+            lines = pcifile.readlines()
+            last = lines[-1]
+            i = 0
+            for line in lines:
+                if line.startswith("#") or line.isspace():
+                    continue
+                if line != last:
+                    headerfile.write("        {{{id}, \"{name}\"}},\n".format(id=i, name=line.strip().replace("\\", "\\\\").replace("\"", "\\\"")))
+                else:
+                    headerfile.write("        {{{id}, \"{name}\"}}".format(id=i, name=line.strip().replace("\\", "\\\\").replace("\"", "\\\"")))
+                i += 1
+        headerfile.write("""
+    };
+    
+    inline const char* lookup(short vendorId, short deviceId)
+    {
+        return names[vendorId];
+    }
+}
+#endif // PCIL_H
+""")
+    if not args.quiet:
+        print("The header file is located at " + path)
